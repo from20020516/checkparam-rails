501be7f8175b2711367f3be4a0eed89838f6e2b9
from20020516 <from20020516@gmail.com>
2019-01-01 12:06:29 +0900

    Add columns to description table.

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,17 +3,24 @@
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
+# 日本語doc:
+# https://morizyun.github.io/blog/whenever-gem-rails-ruby-capistrano/
+# 設定:
+# bundle exec whenever --update-crontab
+# 削除:
+# bundle exec whenever --clear-crontab
+
+set :output, 'log/crontab.log'
+set :environment, :production
+
+every :monday, at: '5am' do # Use any day of the week or :weekend, :weekday
+  command 'sudo certbot-auto renew --post-hook "sudo service nginx restart"'
+end
+
+# every 1.minute do # 1.minute 1.day 1.week 1.month 1.year is also supported
+#   runner "parse:color"
 # end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
+
+# every :day, at: '5am' do # Many shortcuts available: :hour, :day, :month, :year, :reboot
+#   runner ''
 # end
-# Learn more: http://github.com/javan/whenever
--- db/migrate/20181230163522_create_descriptions.rb
+++ db/migrate/20181230163522_create_descriptions.rb
@@ -3,6 +3,9 @@ class CreateDescriptions < ActiveRecord::Migration[5.2]
     create_table :descriptions do |t|
       t.text    :ja
       t.text    :en
+      t.integer :"Ｄ"
+      t.integer :"隔"
+      t.integer :"魔法ダメージ"
       t.integer :HP
       t.integer :MP
       t.integer :STR
@@ -12,6 +15,42 @@ class CreateDescriptions < ActiveRecord::Migration[5.2]
       t.integer :INT
       t.integer :MND
       t.integer :CHR
+      t.integer :"攻"
+      t.integer :"飛攻"
+      t.integer :"魔攻"
+      t.integer :"防"
+      t.integer :"魔防"
+      t.integer :"命中"
+      t.integer :"飛命"
+      t.integer :"魔命"
+      t.integer :"魔命スキル"
+      t.integer :"回避"
+      t.integer :"受け流しスキル"
+      t.integer :"魔回避"
+      t.integer :"被ダメージ"
+      t.integer :"被物理ダメージ"
+      t.integer :"被魔法ダメージ"
+      t.integer :"ダブルアタック"
+      t.integer :"トリプルアタック"
+      t.integer :"クワッドアタック"
+      t.integer :"クリティカルヒット"
+      t.integer :"クリティカルヒットダメージ"
+      t.integer :"ウェポンスキルのダメージ"
+      t.integer :"マジックバーストダメージ"
+      t.integer :"被クリティカルヒット"
+      t.integer :"ヘイスト"
+      t.integer :"二刀流"
+      t.integer :"ファストキャスト"
+      t.integer :"スナップショット"
+      t.integer :"ラピッドショット"
+      t.integer :"敵対心"
+      t.integer :"ストアTP"
+      t.integer :"モクシャ"
+      t.integer :"リジェネ"
+      t.integer :"リフレシュ"
+      t.integer :"コンサーブMP"
+      t.integer :"ケアル回復量"
+      t.integer :"ケアル回復量II"
       t.timestamps
     end
   end
