1141a3679a464faa2a3ac904c12e0d856fd9d510
from20020516 <from20020516@gmail.com>
2019-01-10 20:23:04 +0900

    Add default icon for when gear unselected.

--- app/models/gearset.rb
+++ app/models/gearset.rb
@@ -1,3 +1,16 @@
 class Gearset < ApplicationRecord
   belongs_to :user
+  ## overwrite default attr_accessor??
+  # attr_accessor :id, :main, :sub, :range, :ammo, :head, :neck, :ear1, :ear2, :body, :hands, :ring1, :ring2, :back, :waist, :legs, :feet
+  before_validation :set_gearset
+
+  #require 'byebug'
+
+  private
+  
+  def set_gearset
+    self.attributes[:gearset].each do |key,value|
+      self.attributes[key] = value ? value.to_i : 0
+    end
+  end
 end
