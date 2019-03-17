9c068d1fb3fcb0579860907068b468f82e953ab5
from20020516 <from20020516@gmail.com>
2019-03-17 11:16:02 +0900

    Fix crontab tasks.

--- config/schedule.rb
+++ config/schedule.rb
@@ -10,13 +10,18 @@
 # bundle exec whenever --update-crontab
 # bundle exec whenever --clear-crontab
 
-set :output, 'log/crontab.log'
+# Rails.root PATH
+require File.expand_path(File.dirname(__FILE__) + "/environment")
+set :output, "#{Rails.root}/log/crontab.log"
 set :environment, :production
 
-every :monday, at: '5am' do # Use any day of the week or :weekend, :weekday
+# every 1.minute do # 1.minute 1.day 1.week 1.month 1.year is also supported
+# every :monday, at:'5am' do # :weekend, :weekday
+
+every :monday, at: '5am' do
   command 'sudo certbot-auto renew --post-hook "sudo service nginx restart"'
 end
 
-# every 1.minute do # 1.minute 1.day 1.week 1.month 1.year is also supported
-#   rake "parse:items"
-# end
+every :thursday, at: '5am' do
+  command "cd #{Rails.root}/public && wget https://github.com/ProjectTako/ffxi-addons/raw/master/equipviewer/icons.7z -O icons.7z && 7za x icons.7z -aoa icons/64 && cd ../Resources && git pull && cd .. && export RAILS_ENV=production && rails parse:items"
+end
