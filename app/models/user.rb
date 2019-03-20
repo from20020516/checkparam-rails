73046057cb2f7f37068b20d5e11f08446314657c
from20020516 <from20020516@gmail.com>
2019-03-20 11:46:55 +0900

    Fix header because turbolinks issue.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,6 @@ class User < ApplicationRecord
   has_many :gearsets, dependent: :destroy
   has_one :job, primary_key: :job_id, foreign_key: :id
 
-  ## 以下の2つは同じ意味 # job_id columnがあればjobidを省略可能
-  # belongs_to :job, primary_key: :id, foreign_key: :jobid
-  # has_one :job, primary_key: :jobid, foreign_key: :id
-
   private
 
   def self.find_for_oauth(auth)
@@ -18,17 +14,20 @@ class User < ApplicationRecord
    unless user
      user = User.create(
        uid:      auth.uid,
-       provider: auth.provider,
-       email:    auth.info.email, # require APIs permission.
-       lang:     auth.extra.raw_info.lang == "ja" ? "ja" : "en",
        password: Devise.friendly_token[0, 20],
-       image:    auth.info.image.gsub("http","https"),
-       nickname: auth.info.nickname,
-       name:     auth.info.name,
+       provider: auth.provider,
+       lang:     auth.extra.raw_info.lang == 'ja' ? 'ja' : 'en',
        job_id:   1,
        index:    1,
      )
    end
+
+   user.update(
+     image:     auth.info.image.gsub('http', 'https'),
+     email:     auth.info.email, # require APIs permission.
+     nickname:  auth.info.nickname,
+     name:      auth.info.name,
+    )
 
    user
   end
