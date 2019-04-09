bb7b6f136c7814f5fc186b8e24207d9da3249e11
from20020516 <from20020516@gmail.com>
2019-04-09 21:59:59 +0900

    Add wiki.ffo.jp link for each items.

--- app/controllers/descriptions_controller.rb
+++ app/controllers/descriptions_controller.rb
@@ -1,10 +1,6 @@
 class DescriptionsController < ApplicationController
-  # include ApplicationHelper
   def index
-    item_id = JSON.parse(gearset_params)
-    items = Item.where(id: item_id).pluck(:id, :description).to_h
-    #byebug
-    render json: item_id.map{ |id| [id, items[id.to_i]&.dig(session[:lang])] }.to_h
+    render json: Item.where(id: JSON.parse(gearset_params)).pluck(:id, :description).to_h.merge(lang: session[:lang])
   end
 
   def about
---
+++ app/models/ffo.rb
@@ -0,0 +1,3 @@
+class Ffo < ApplicationRecord
+  has_many :items
+end
