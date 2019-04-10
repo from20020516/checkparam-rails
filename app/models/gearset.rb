f0d607675702b274e506d05e770379a7aa43c5c6
from20020516 <from20020516@gmail.com>
2019-04-10 11:20:34 +0900

    Fix DescriptionsController.

--- app/models/gearset.rb
+++ app/models/gearset.rb
@@ -10,6 +10,7 @@ class Gearset < ApplicationRecord
   end
 
   def checkparam
+    # TODO: 共通化
     stats = Stat.where(id: self.slice(slots.pluck(:name)).values).to_a
     stat_names = Stat.column_names - %w[id created_at updated_at HP MP 防 Ｄ 隔 STR DEX AGI VIT INT MND CHR]
     stat_names.map { |stat_name| [stat_name, stats.pluck(stat_name).compact.sum] }.to_h
