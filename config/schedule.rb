21a4d50f5dd05d0c999142171439d6c4bbd73a83
from20020516 <from20020516@gmail.com>
2019-04-18 23:17:41 +0900

    Update crontab jobs.

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,9 +6,13 @@
 # docs:
 # https://morizyun.github.io/blog/whenever-gem-rails-ruby-capistrano/
 # https://github.com/javan/whenever
-#
-# bundle exec whenever --update-crontab
-# bundle exec whenever --clear-crontab
+
+# write job:
+# sudo -E bundle exec whenever -w
+# clear job:
+# sudo -E bundle exec whenever -c
+# check job:
+# sudo crontab -l
 
 # Rails.root PATH
 require File.expand_path("#{File.dirname(__FILE__)}/environment")
@@ -18,21 +22,15 @@ set :environment, :production
 # every 1.minute do # 1.minute 1.day 1.week 1.month 1.year is also supported
 # every :monday, at:'5am' do # :weekend, :weekday
 
-every :monday, at: '5am' do
+every 1.day, at: '5am' do
   command 'sudo certbot-auto renew --post-hook "sudo service nginx restart"'
 end
 
 every :thursday, at: '5am' do
-  command "
-    cd #{Rails.root} &&
-    wget https://github.com/ProjectTako/ffxi-addons/raw/master/equipviewer/icons.7z -O public/icons.7z &&
-    7za e public/icons.7z -aos -opublic/icons/ icons/64/*.png &&
-    git submodule foreach git pull origin master &&
-    export RAILS_ENV=production &&
-    rails csv:export &&
-    rails parse:items &&
-    bin/rails assets:precompile &&
-    kill -9 `pgrep -f puma` &&
-    bin/rails s -p 8080 -e production -d
-  "
+  command "cd #{Rails.root} && wget https://github.com/ProjectTako/ffxi-addons/raw/master/equipviewer/icons.7z -O public/icons.7z && 7za e public/icons.7z -aos -opublic/icons/ icons/64/*.png && git submodule foreach git pull origin master && export RAILS_ENV=production && rails csv:export && rails parse:items && bin/rails assets:precompile && kill -9 `pgrep -f puma` && bin/rails s -p 3000 -e production -d"
 end
+
+# test
+# every 1.minute do
+#   command "sudo service nginx restart >>#{Rails.root.join('log', 'cron_success.log')} 2>>#{Rails.root.join('log', 'cron_error.log')}"
+# end
