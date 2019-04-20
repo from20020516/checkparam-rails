f52ba5b20b4039fe47ff0be43944eaa3cb89368b
from20020516 <from20020516@gmail.com>
2019-04-21 00:36:31 +0900

    Fix card partial.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.log_level = :info
+  # config.log_level = :info
 
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
