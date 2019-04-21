9f343ee148a09a3f420b9ae262d0498e422ca586
from20020516 <from20020516@gmail.com>
2019-04-22 00:47:59 +0900

    Fix parser.

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -1,11 +1,21 @@
 class Stat < ApplicationRecord
   belongs_to :item
+  before_update :prepare_update
 
   scope :names, -> { column_names - %w[id item_id created_at updated_at Ｄ] }
   scope :current, ->(*ids) { where(item_id: ids) }
 
   # TODO: Non-attribute arguments will be disallowed in Rails 6.0. This method should not be called with user-provided values, such as request parameters or model attributes. Known-safe values can be passed by wrapping them in Arel.sql().
   scope :checkparam, -> (*ids) {
-    names.map(&:to_sym).zip(current(ids).pluck(names).transpose.map(&:compact).map(&:compact).map(&:sum)).to_h
+    names.map(&:to_sym).zip(current(ids).pluck(names).transpose.map(&:sum)).to_h
   }
+
+  private
+
+  def prepare_update
+    # if changed?
+    #   puts item[:ja], item[:description][:raw], changes_to_save
+    #   byebug
+    # end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_20_190526) do
+ActiveRecord::Schema.define(version: 2019_04_23_034616) do
 
   create_table "gearsets", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -61,71 +61,71 @@ ActiveRecord::Schema.define(version: 2019_04_20_190526) do
   end
 
   create_table "stats", primary_key: "item_id", force: :cascade do |t|
-    t.integer "HP"
-    t.integer "MP"
-    t.integer "STR"
-    t.integer "DEX"
-    t.integer "VIT"
-    t.integer "AGI"
-    t.integer "INT"
-    t.integer "MND"
-    t.integer "CHR"
-    t.integer "Ｄ"
-    t.integer "隔"
-    t.integer "防"
-    t.integer "命中"
-    t.integer "飛命"
-    t.integer "魔命"
-    t.integer "攻"
-    t.integer "飛攻"
-    t.integer "魔攻"
-    t.integer "回避"
-    t.integer "魔回避"
-    t.integer "魔防"
-    t.integer "ヘイスト"
-    t.integer "敵対心"
-    t.integer "被ダメージ"
-    t.integer "被物理ダメージ"
-    t.integer "被魔法ダメージ"
-    t.integer "ストアTP"
-    t.integer "ファストキャスト"
-    t.integer "精霊魔法の詠唱時間"
-    t.integer "回復魔法の詠唱時間"
-    t.integer "ケアル詠唱時間"
-    t.integer "魔法ダメージ"
-    t.integer "魔法剣ダメージ"
-    t.integer "ファランクス"
-    t.integer "マジックバーストダメージ"
-    t.integer "マジックバーストダメージII"
-    t.integer "二刀流"
-    t.integer "ダブルアタック"
-    t.integer "トリプルアタック"
-    t.integer "クワッドアタック"
-    t.integer "ダブルショット"
-    t.integer "トリプルショット"
-    t.integer "クワッドショット"
-    t.integer "スナップショット"
-    t.integer "ラピッドショット"
-    t.integer "ウェポンスキルのダメージ"
-    t.integer "ケアル回復量"
-    t.integer "ケアル回復量II"
-    t.integer "被ケアル回復量"
-    t.integer "クリティカルヒット"
-    t.integer "クリティカルヒットダメージ"
-    t.integer "ペット_命中"
-    t.integer "ペット_魔命"
-    t.integer "ペット_攻"
-    t.integer "ペット_魔攻"
-    t.integer "ペット_ダブルアタック"
-    t.integer "ペット_リジェネ"
-    t.integer "ペット_被ダメージ"
-    t.integer "ペット_被物理ダメージ"
-    t.integer "ペット_被魔法ダメージ"
-    t.integer "契約の履行使用間隔"
-    t.integer "契約の履行使用間隔II"
-    t.integer "ペット_契約の履行ダメージ"
-    t.integer "召喚獣維持費"
-    t.integer "詠唱中断率"
+    t.integer "HP", default: 0
+    t.integer "MP", default: 0
+    t.integer "STR", default: 0
+    t.integer "DEX", default: 0
+    t.integer "VIT", default: 0
+    t.integer "AGI", default: 0
+    t.integer "INT", default: 0
+    t.integer "MND", default: 0
+    t.integer "CHR", default: 0
+    t.integer "Ｄ", default: 0
+    t.integer "隔", default: 0
+    t.integer "防", default: 0
+    t.integer "命中", default: 0
+    t.integer "飛命", default: 0
+    t.integer "魔命", default: 0
+    t.integer "攻", default: 0
+    t.integer "飛攻", default: 0
+    t.integer "魔攻", default: 0
+    t.integer "回避", default: 0
+    t.integer "魔回避", default: 0
+    t.integer "魔防", default: 0
+    t.integer "ヘイスト", default: 0
+    t.integer "敵対心", default: 0
+    t.integer "被ダメージ", default: 0
+    t.integer "被物理ダメージ", default: 0
+    t.integer "被魔法ダメージ", default: 0
+    t.integer "ストアTP", default: 0
+    t.integer "ファストキャスト", default: 0
+    t.integer "精霊魔法の詠唱時間", default: 0
+    t.integer "回復魔法の詠唱時間", default: 0
+    t.integer "ケアル詠唱時間", default: 0
+    t.integer "魔法ダメージ", default: 0
+    t.integer "魔法剣ダメージ", default: 0
+    t.integer "ファランクス", default: 0
+    t.integer "マジックバーストダメージ", default: 0
+    t.integer "マジックバーストダメージII", default: 0
+    t.integer "二刀流", default: 0
+    t.integer "ダブルアタック", default: 0
+    t.integer "トリプルアタック", default: 0
+    t.integer "クワッドアタック", default: 0
+    t.integer "ダブルショット", default: 0
+    t.integer "トリプルショット", default: 0
+    t.integer "クワッドショット", default: 0
+    t.integer "スナップショット", default: 0
+    t.integer "ラピッドショット", default: 0
+    t.integer "ウェポンスキルのダメージ", default: 0
+    t.integer "ケアル回復量", default: 0
+    t.integer "ケアル回復量II", default: 0
+    t.integer "被ケアル回復量", default: 0
+    t.integer "クリティカルヒット", default: 0
+    t.integer "クリティカルヒットダメージ", default: 0
+    t.integer "ペット_命中", default: 0
+    t.integer "ペット_魔命", default: 0
+    t.integer "ペット_攻", default: 0
+    t.integer "ペット_魔攻", default: 0
+    t.integer "ペット_ダブルアタック", default: 0
+    t.integer "ペット_リジェネ", default: 0
+    t.integer "ペット_被ダメージ", default: 0
+    t.integer "ペット_被物理ダメージ", default: 0
+    t.integer "ペット_被魔法ダメージ", default: 0
+    t.integer "契約の履行使用間隔", default: 0
+    t.integer "契約の履行使用間隔II", default: 0
+    t.integer "ペット_契約の履行ダメージ", default: 0
+    t.integer "召喚獣維持費", default: 0
+    t.integer "詠唱中断率", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
